7d1130be268ec2eaf0cf1143f73d7c9a14eb059f
Omer Ied <omeried73@gmail.com>
2021-05-24 17:49:01 +0300

    link module done

--- link.py
+++ link.py
@@ -1,14 +1,27 @@
+import abc
+
 
 class Link:
+    @abc.abstractmethod
     def compute(self, cluster, other):
-        pass
+        return
 
 
 class SingleLink(Link):
     def compute(self, cluster, other):
-        pass
+        min_d = cluster.samples[0].compute_euclidean_distance(other.samples[0])
+        for s_cluster in cluster.samples:
+            for s_other in other.samples:
+                if s_cluster.compute_euclidean_distance(s_other) < min_d:
+                    min_d = s_cluster.compute_euclidean_distance(s_other)
+        return min_d
 
 
 class CompleteLink(Link):
     def compute(self, cluster, other):
-        pass
+        max_d = cluster.samples[0].compute_euclidean_distance(other.samples[0])
+        for s_cluster in cluster.samples:
+            for s_other in other.samples:
+                if s_cluster.compute_euclidean_distance(s_other) > max_d:
+                    max_d = s_cluster.compute_euclidean_distance(s_other)
+        return max_d
